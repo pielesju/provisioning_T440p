fe48819c80b15048cef03b8e5fd9e516aef55140
Julian Pieles <julian.pieles@kzvbw.de>
2024-02-29 11:56:59 +0100

    .

--- provision.sh
+++ provision.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+###########
 
-echo "Hello World!"
+printf "STARTING PROVISIONING\n"
 
-sudo pacman -Syu
-sudo pacman -S ansible
+function task {
+    NAME=$1
+    CMD=$2
+    
+    printf "%s\n" $NAME
+    eval "$CMD" &> /dev/null
+    if [[ $? != 0 ]]; then
+        printf "%s FAILED\n" $NAME
+        exit 1
+    fi
+}
+
+################################################################################
+
+task "UPDATE SYSTEM" "sudo pacman -Syu"
+
+task "INSTALL PYTHON" "sudo pacman -S python"
+
+task "INSTALL ANSIBLE" "sudo pacman -S ansible &> /dev/null"
+
+printf "PULL AND EXECUTE PLAYBOOK"
+sudo ansible-pull -U https://github.com/pielesju/provisioning_T440p.git t440p.yml
+
+exit 0
